4a481f271d4a20a7eefe9f15c0ad94381d0a5bb1
Kyle Ryan <kyle.ryan@cognizant.com>
2020-03-28 18:31:01 -0500

    Kyle - spring-bootification-work

---
+++ src/main/java/org/superbiz/moviefun/HomeController.java
@@ -0,0 +1,47 @@
+package org.superbiz.moviefun;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+
+@Controller
+public class HomeController {
+
+    private final MoviesBean moviesBean;
+
+    @Autowired
+    public HomeController(MoviesBean moviesBean) {
+        Assert.notNull(moviesBean, "moviesBean must not be null");
+        this.moviesBean = moviesBean;
+    }
+
+    @GetMapping("/setup")
+    public String setup(Map<String, Object> model) {
+        moviesBean.addMovie(new Movie("Wedding Crashers", "David Dobkin", "Comedy", 7, 2005));
+        moviesBean.addMovie(new Movie("Starsky & Hutch", "Todd Phillips", "Action", 6, 2004));
+        moviesBean.addMovie(new Movie("Shanghai Knights", "David Dobkin", "Action", 6, 2003));
+        moviesBean.addMovie(new Movie("I-Spy", "Betty Thomas", "Adventure", 5, 2002));
+        moviesBean.addMovie(new Movie("The Royal Tenenbaums", "Wes Anderson", "Comedy", 8, 2001));
+        moviesBean.addMovie(new Movie("Zoolander", "Ben Stiller", "Comedy", 6, 2001));
+        moviesBean.addMovie(new Movie("Shanghai Noon", "Tom Dey", "Comedy", 7, 2000));
+
+        model.put("movies", moviesBean.getMovies());
+        return "setup";
+    }
+
+    @RequestMapping("/")
+    String index() {
+        return "index";
+    }
+
+    @RequestMapping("/setup")
+    String setup() {
+        return "setup";
+    }
+}
